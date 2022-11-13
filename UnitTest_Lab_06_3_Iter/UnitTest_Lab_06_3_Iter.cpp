71d3cfaf59672885f927d3d48c6f0bdb9d8f33a8
Glit <vpad.lazar.04@gmail.com>
2022-11-13 18:18:12 +0200

    Add project files.

---
+++ UnitTest_Lab_06_3_Iter/UnitTest_Lab_06_3_Iter.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06_3_Iter/Lab_06_3_Iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+void Test(int* a, int* c, const int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int min = c[i];
+		int imin = i;
+		for (int j = i + 1; j < size; j++)
+			if (min > c[j])
+			{
+				min = c[j];
+				imin = j;
+			}
+		c[imin] = c[i];
+		c[i] = min;
+	}
+	for (int i = 0; i < size; i++)
+		Assert::AreEqual(a, c);
+}
+
+namespace UnitTestLab063Iter
+{
+	TEST_CLASS(UnitTestLab063Iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int c[n];
+			int Low = -10, High = 10;
+			Create(c, n, Low, High);
+			int* a = Sort(c, n);
+			Test(a, c, n);
+		}
+	};
+}
